79026dbf88f961314eb4af0577eff4230d4ec6a0
Deividas <web@dkvadratu.lt>
2025-04-11 15:21:27 +0300

    Newest code. NPM BUILD

--- public/build/assets/LayoutInstallation-DTv_uzfN.js
+++
@@ -1,1 +0,0 @@
-import{N as t}from"./NotificationRoot-BaZv9i9D.js";import{c as a,a as o,b as s,r,o as c}from"./main-DlOnIu1m.js";const n={class:"h-screen overflow-y-auto text-base"},_={class:"container mx-auto px-4"},d={__name:"LayoutInstallation",setup(i){return(l,m)=>{const e=r("router-view");return c(),a("div",n,[o(t),s("div",_,[o(e)])])}}};export{d as default};
